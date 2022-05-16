89520ac4dc875119e5c22f7dfb51bc9355c88fbd
niloufar_alinezhad <niloufar_alinezhad@fdeshragh.com>
2022-05-16 14:53:48 +0430

    test and code button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [buttonColor,setButtonColor]=useState('red');
+  const newButtonColor=buttonColor == 'red'? 'blue': 'red';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button style={{backgroundColor:buttonColor}} onClick={()=>setButtonColor(newButtonColor)}>change to {newButtonColor}</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen ,fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('button has correct initial color', () => {
+  render(<App /> );
 
-  expect(linkElement).toBeInTheDocument();
+  //find an element with role button and text change to blue
+  const colorButton=screen.getByRole('button',{name:'change to blue'});
+  
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({background:'red'});
+  //click button
+  fireEvent.click(colorButton);
+  //expect  the background color to be blue
+  expect(colorButton).toHaveStyle({background:'blue'});
+
+  //expect the text to be 'change to red'
+  expect(colorButton.textContent).toBe('change to red')
 });
+
+
